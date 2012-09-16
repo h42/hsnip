8f032be4fdf148fed8e58e1fc1b0757c9e391e7f
h42 <jdinardo@nycap.rr.com>
2012-09-15 22:21:10 -0400

    added matMult

--- ffi/hmat.hs
+++ ffi/hmat.hs
@@ -5,11 +5,19 @@ module Main (
     ,newMat
     ,fromList
     ,toList
-    ,getMatElem
-    ,putMatElem
+    ,getBuf
+    ,getRows
+    ,getCols
+    ,getElem
+    ,putElem
     ,addMat
+    ,multMat
+    ,incMat
+    ,scaleMat
     ,idMat
     ,zeroMat
+    ,transMat
+    ,showMat
 ) where
 
 import Control.Monad
@@ -23,23 +31,26 @@ import MY.Show
     ,buf  :: (Ptr CDouble)
 }-}
 
-foreign import ccall "getMatBuf" getMatBuf   :: Ptr a ->  IO (Ptr Double)
-foreign import ccall "getMatElem" c_getElem
-    :: Ptr a ->  CInt -> CInt -> IO Double
-foreign import ccall "putMatElem" c_putElem
+foreign import ccall "getBuf" getBuf   :: Ptr a ->  IO (Ptr CDouble)
+foreign import ccall "getRows" getRows :: Ptr a ->  IO CInt
+foreign import ccall "getCols" getCols :: Ptr a ->  IO CInt
+foreign import ccall "getElem" c_getElem  :: Ptr a ->  CInt -> CInt -> IO Double
+foreign import ccall "putElem" c_putElem
     :: Ptr a ->  CInt -> CInt -> CDouble -> IO ()
-foreign import ccall "getMatRows" getMatRows :: Ptr a ->  IO CInt
-foreign import ccall "getMatCols" getMatCols :: Ptr a ->  IO CInt
 foreign import ccall "addMat" addMat         :: Ptr a -> Ptr a -> IO ()
+foreign import ccall "multMat" c_multMat
+    :: Ptr a -> Ptr a -> Ptr a -> IO (CInt)
+foreign import ccall "incMat" c_incMat       :: Ptr a -> CDouble -> IO ()
+foreign import ccall "scaleMat" c_scaleMat   :: Ptr a -> CDouble -> IO ()
 foreign import ccall "idMat" idMat           :: Ptr a -> IO ()
 foreign import ccall "newMat" newMat_c  :: CInt -> CInt ->  IO (Ptr a)
 foreign import ccall "transMat" transMat       :: Ptr a -> IO ()
 foreign import ccall "zeroMat" zeroMat       :: Ptr a -> IO ()
 
-getMatElem m r c =
+getElem m r c =
     fmap realToFrac $ c_getElem m (fromIntegral r) (fromIntegral c)
 
-putMatElem m r c d =
+putElem m r c d =
     c_putElem m (fromIntegral r) (fromIntegral c) (realToFrac d)
 
 newMat :: Int -> Int -> IO (Ptr a)
@@ -48,23 +59,23 @@ newMat r c = newMat_c (fromIntegral r) (fromIntegral c)
 fromList :: Int -> Int -> [Double] -> IO (Ptr a)
 fromList r c xs = do
     m <- newMat r c
-    ptr <- getMatBuf m
+    ptr <- getBuf m
     --ptr <- mallocArray (r*c) :: IO (Ptr CDouble)
     when (not.null $ xs) $ pokeArray ptr (map realToFrac (take (r*c) xs))
     return m
 
 toList :: Ptr a -> IO [Double]
 toList m = do
-    r <- getMatRows m
-    c <- getMatCols m
-    ptr <- getMatBuf m
+    r <- getRows m
+    c <- getCols m
+    ptr <- getBuf m
     xs <- (peekArray (fromIntegral (r*c)) ptr)
     return $ map realToFrac xs
 
 showMat m dplaces = do
-    r <- getMatRows m
-    c <- getMatCols m
-    ptr <- getMatBuf m
+    r <- getRows m
+    c <- getCols m
+    ptr <- getBuf m
     a <- peekArray (fromIntegral $ r*c) ptr
     let l = 7 -- dp + 1 + (length $ show $ ceiling $ maximum a)
 	a2 = map (fshow (l,dplaces)) a
@@ -75,22 +86,35 @@ showCR (x:xs) i c
     | i /= c = x : showCR xs (i+1) c
     | otherwise = x: "\n" : showCR xs 1 c
 
+multMat m m1 m2 = do
+    rc <- c_multMat m m1 m2
+    let ans = if rc /=0 then Nothing else Just rc where
+    return ans
+
+incMat m x = c_incMat m (realToFrac x)
+scaleMat m x = c_scaleMat m (realToFrac x)
+
 main = do
-    m <- fromList 4 4 [1,3..]
+    m <- fromList 2 2 [1,3..]
     addMat m m
-    ds <- toList m
-    print ds
+    --ds <- toList m
+    --print ds
+    showMat m 1 >>= putStrLn
+    scaleMat m 0.5 >> showMat m 1 >>= putStrLn
+    incMat m (-2) >> showMat m 1 >>= putStrLn
+    transMat m >> showMat m 1 >>= putStrLn
+    zeroMat m >> showMat m 1 >>= putStrLn
+    idMat m >> showMat m 1 >>= putStrLn
+    m1 <- fromList 2 2 [1..]
+    m2 <- fromList 2 2 [1..]
+    transMat m2
+    m3 <- newMat 2 2
+    showMat m1 1 >>= putStrLn
+    showMat m2 1 >>= putStrLn
+    multMat m3 m1 m2  >> showMat m3 1 >>= putStrLn
     {-
     put m 2 2 27.1
     get m 0 3 >>= print
     get m 2 2 >>= print
-    m2 <- fromList 4 4 [2,4..]
-    addMat m m2
-    toList m >>= print
-    showMat m 0 >>= putStrLn
-    transMat m
-    showMat m 0 >>= putStrLn
-    idMat m
-    showMat m 0 >>= putStrLn
     -}
     return ()
--- ffi/hmat_c.c
+++ ffi/hmat_c.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <malloc.h>
 
+#define ij (i*c+j)
+#define ji (j*c+i)
+
 typedef struct Matrix {
     int rows,cols;
     double *dv;
@@ -14,38 +17,63 @@ void *newMat(int r, int c) {
     return vp;
 }
 
-double *getMatBuf(Matrix *m) {
+double *getBuf(Matrix *m) {
     return m->dv;
 }
 
-int getMatRows(Matrix *m) {
+int getRows(Matrix *m) {
     return m->rows;
 }
 
-int getMatCols(Matrix *m) {
+int getCols(Matrix *m) {
     return m->cols;
 }
 
-double getMatElem(Matrix *m, int i, int j) {
+double getElem(Matrix *m, int i, int j) {
     return m->dv[i * m->cols + j];
 }
 
-void putMatElem(Matrix *m, int i, int j, double d) {
+void putElem(Matrix *m, int i, int j, double d) {
     m->dv[i * m->cols + j] = d;
 }
 
 void addMat(Matrix *m, Matrix *m2) {
-    int i;
-    for (i=0; i<m->rows * m->cols; i++) {
-	m->dv[i] += m2->dv[i];
+    int i, n = m->rows * m->cols;
+    double *dv=m->dv, *dv2=m2->dv;
+    for (i=0; i<n; i++) dv[i] += dv2[i];
+}
+
+int multMat(Matrix *p, Matrix *m, Matrix *m2) {
+    int i,j,k,a;
+    int r=m->rows,c=m->cols,r2=m2->rows;
+    if (c != r2 || p==m || p==m2) return -1;
+    double *dv=m->dv, *dv2=m2->dv, *product=p->dv;
+    for (i=0; i<r; i++) {
+	for (j=0; j<c; j++) {
+	    for (k=0,a=0; k<c; k++) a += dv[i*c+k] * dv2[k*c+j];
+	    product[ij] = a;
+	}
     }
+    return 0;
+}
+
+void incMat(Matrix *m, double x) {
+    int i,n=m->rows*m->cols;
+    double *dv = m->dv;
+    for (i=0;i<n;i++) dv[i]+=x;
+}
+
+void scaleMat(Matrix *m, double x) {
+    int i,n=m->rows*m->cols;
+    double *dv = m->dv;
+    for (i=0;i<n;i++) dv[i]*=x;
 }
 
 void idMat(Matrix *m) {
     int i,j,c=m->cols,r=m->rows;
     double *dv = m->dv;
     for (i=0; i<r; i++) {
-	for (j=0; j<c; j++) dv[i*c+j] = (i==j) ? 1 : 0;
+	for (j=0; j<c; j++) dv[ij] = (i==j) ? 1 : 0;
     }
 }
 
@@ -55,19 +83,16 @@ void zeroMat(Matrix *m) {
     for (i=0; i<r*c; i++) dv[i]=0;
 }
 
-void _transMat(int r, int c, double m[r][c]) {
+void transMat(Matrix *m) {
     int i,j;
-    double t;
+    int r=m->rows, c=m->cols;
+    double t, *dv=m->dv;
     for (i=0; i<r; i++) {
 	for (j=0; j<i; j++) {
-	    t=m[i][j];
-	    m[i][j] = m[j][i];
-	    m[j][i] = t;
+	    t=dv[ij];
+	    dv[ij] = dv[ji];
+	    dv[ji] = t;
 	}
     }
-}
-
-void transMat(Matrix *m) {
-    _transMat(m->rows, m->cols, m->dv);
 }
 
